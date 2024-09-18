3c2b201e04700eeb85a4283ce1624dc13c024f31
poojs <poojasurendran40@gmail.com>
2024-09-18 21:44:53 +0530

    fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ const nav__links = [
     display: "Image"
   },
   {
-    path: 'https://docs-ai-three.vercel.app/', // Modify this path to match your Next.js application URL
+    path: "https://ai-doc-con.vercel.app/", // Modify this path to match your Next.js application URL
     display: "Docs"
   }
   // {
